0a8f4806332c9c15ebcc565fd7d437e7b3681abe
quyethdvn1412 <quyet.sneep.hut@gmail.com>
2022-09-26 21:39:43 +0900

    update

--- 1/assinment01/asigm_1_proj/asigm_1_app/urls.py
+++ 1/assinment01/asigm_1_proj/asigm_1_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'asigm_1_app'
 urlpatterns = [
     path('', views.index, name='index'),
     # Display all people
--- 1/assinment01/asigm_1_proj/asigm_1_app/views.py
+++ 1/assinment01/asigm_1_proj/asigm_1_app/views.py
@@ -1,15 +1,22 @@
 from .models import Person
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.template import loader
 # Create your views here.
 
 def index(request):
-    listname = Person.objects.order_by('-name')
-    ouput = '\n '.join(q.name for q in listname)
-    return HttpResponse(ouput)
+    list_people = Person.objects.order_by('-name')
+    template = loader.get_template('asigm_1_app/index.html')
+    content = {
+        'list_people' : list_people
+    }
+    return HttpResponse(template.render(content, request))
 
 def detail(request, id):
-    list = Person.objects()
-    output = '\n'.join()
-    return HttpResponse(output)
+    try:
+        info = Person.objects.get(pk=id)
+    except Person.DoesNotExist:
+        raise Http404('The people of %s does not exist.', id)
+    return render(request, 'asigm_1_app/detail.html', {'id' :id})
     
